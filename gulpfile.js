2bdda3c53bece5cd492ce446ded5d2860d986d68
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-07-12 15:24:19 +0900

    bug fix for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,24 @@ var sass = require('gulp-sass');
 var csso = require('gulp-csso');
 
 gulp.task('js', function(){
-	return gulp.src(["src/*.js"])
-		.pipe(concat('script.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./js'));
+  return gulp.src(["src/*.js"])
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'));
 });
 
 gulp.task('sass', function () {
-	return gulp.src('./src/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./src'));
+  return gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src'));
 });
 
-gulp.task('css', gulp.series( gulp.parallel('sass')), function(){
-	return gulp.src(["src/*.css"])
-		.pipe(concat('style.min.css'))
-		.pipe(minifyCSS())
-		.pipe(csso())
-		.pipe(gulp.dest('./css'));
+gulp.task('css', ['sass'], function(){
+  return gulp.src(["src/*.css"])
+    .pipe(concat('style.min.css'))
+    .pipe(minifyCSS())
+    .pipe(csso())
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', gulp.series( gulp.parallel('js', 'css')));
+gulp.task('default', ['js', 'css']);
