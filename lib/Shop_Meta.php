37dd2d0b722d4fda0719a8ffe79e8ea6a49e9b1d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-07-14 15:54:28 +0900

    improve labels

--- lib/Shop_Meta.php
+++ lib/Shop_Meta.php
@@ -5,6 +5,24 @@ use Miya;
 
 class Shop_Meta extends Miya\WP\Custom_Field
 {
+    protected $labels;
+
+    public function __construct( $id, $title, array $options = array() ) {
+	    $this->labels = apply_filters( 'shop_meta_labels', array(
+		    'zip'          => '郵便番号',
+		    'address'      => '住所',
+		    'tel'          => '電話番号',
+		    'url'          => 'URL',
+		    'open'         => '営業時間',
+		    'holiday'      => '定休日',
+		    'parking'      => '駐車場',
+		    'reserveation' => '予約',
+		    'note'         => '備考',
+	    ) );
+
+	    parent::__construct( $id, $title, $options );
+    }
+
 	/**
 	 * Fires at the `admin_enqueue_scripts` hook.
 	 *
@@ -29,17 +47,21 @@ class Shop_Meta extends Miya\WP\Custom_Field
 	public function form( $post, $args )
 	{
 		?>
+        <h3>連絡先</h3>
 		<table class="shop-meta">
-			<tr><th>郵便番号</th><td><input type="text" name="zip" value="<?php $this->_meta( '_zip' ) ?>"></td></tr>
-			<tr><th>住所</th><td><input type="text" name="address" value="<?php $this->_meta( '_address' ) ?>"></td></tr>
-			<tr><th>電話番号</th><td><input type="text" name="tel" value="<?php $this->_meta( '_tel' ) ?>"></td></tr>
-			<tr><th>URL</th><td><input type="text" name="url" value="<?php $this->_meta( '_url' ) ?>"></td></tr>
-			<tr><th>営業時間</th><td><input type="text" name="open" value="<?php $this->_meta( '_open' ) ?>"></td></tr>
-			<tr><th>定休日</th><td><input type="text" name="holiday" value="<?php $this->_meta( '_holiday' ) ?>"></td></tr>
-			<tr><th>駐車場</th><td><input type="text" name="parking" value="<?php $this->_meta( '_parking' ) ?>"></td></tr>
-			<tr><th>予約</th><td><input type="text" name="reserveation" value="<?php $this->_meta( '_reserveation' ) ?>"></td></tr>
-			<tr><th>人気メニュー</th><td><input type="text" name="popular" value="<?php $this->_meta( '_popular' ) ?>"></td></tr>
-            <tr><th>備考</th><td><textarea name="note"><?php echo esc_textarea( get_post_meta( get_the_ID(), '_note', true ) ); ?></textarea></td></tr>
+			<tr><th><?php label( 'zip' ); ?></th><td><input type="text" name="zip" value="<?php $this->_meta( '_zip' ) ?>"></td></tr>
+			<tr><th><?php label( 'address' ); ?></th><td><input type="text" name="address" value="<?php $this->_meta( '_address' ) ?>"></td></tr>
+			<tr><th><?php label( 'tel' ); ?></th><td><input type="text" name="tel" value="<?php $this->_meta( '_tel' ) ?>"></td></tr>
+			<tr><th><?php label( 'url' ); ?></th><td><input type="text" name="url" value="<?php $this->_meta( '_url' ) ?>"></td></tr>
+        </table>
+
+        <h3>営業案内</h3>
+        <table class="shop-meta">
+			<tr><th><?php label( 'open' ); ?></th><td><input type="text" name="open" value="<?php $this->_meta( '_open' ) ?>"></td></tr>
+			<tr><th><?php label( 'holiday' ); ?></th><td><input type="text" name="holiday" value="<?php $this->_meta( '_holiday' ) ?>"></td></tr>
+			<tr><th><?php label( 'parking' ); ?></th><td><input type="text" name="parking" value="<?php $this->_meta( '_parking' ) ?>"></td></tr>
+			<tr><th><?php label( 'reserveation' ); ?></th><td><input type="text" name="reserveation" value="<?php $this->_meta( '_reserveation' ) ?>"></td></tr>
+            <tr><th><?php label( 'note' ); ?></th><td><textarea name="note"><?php echo esc_textarea( get_post_meta( get_the_ID(), '_note', true ) ); ?></textarea></td></tr>
 		</table>
 		<?php
 	}
@@ -51,7 +73,7 @@ class Shop_Meta extends Miya\WP\Custom_Field
 	 */
 	public function save( $post_id )
 	{
-		$keys = array( 'zip', 'address', 'tel', 'url', 'open', 'holiday', 'parking', 'note', '_reserveation', 'popular' );
+		$keys = array( 'zip', 'address', 'tel', 'url', 'open', 'holiday', 'parking', 'note', '_reserveation', 'note' );
 		foreach ( $keys as $key ) {
 		    if ( empty( $_POST[ $key ] ) ) {
 			    update_post_meta( get_the_ID(), '_' . $key, "" );
@@ -65,4 +87,9 @@ class Shop_Meta extends Miya\WP\Custom_Field
 	{
 		echo esc_attr( get_post_meta( get_the_ID(), $key, true ) );
 	}
+
+	protected function label( $key )
+    {
+        echo esc_html( $this->labels[ $key ] );
+    }
 }
